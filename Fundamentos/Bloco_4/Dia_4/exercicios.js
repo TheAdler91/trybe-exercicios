7afebb2954a7aacd3be3e9ee20b7c6c8ef9d1601
Philip Adler <philip.adler@hotmail.com>
2022-05-19 15:00:32 -0300

    Dia 4

---
+++ Fundamentos/Bloco_4/Dia_4/exercicios.js
@@ -0,0 +1,46 @@
+//let info = {
+   // personagem: 'Margarida',
+    //origem: 'Pato Donald',
+    //nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+ // };
+//info.recorrente =  'Sim'
+  //console.log('Bem-Vinda,' + " " + info.personagem) //
+
+  //
+
+  /*let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+info.recorrente =  'Sim'
+console.log(info)*/
+
+/*let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  for (let index in info){
+    console.log(index, info[index])
+
+  }*/
+
+  let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+let infoDois = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christimas on bear mountain, Dells f four color comics #178',
+    nota: 'O último MacPatinhas',
+};
+
+for( let index in info){
+    for( let indexDois in infoDois){
+
+    }
+    
+} console.log(info[index], infoDois[indexDois])
