89622574aae57e09ca38560bd7403954a92c6766
Your NamePhilip <philip.adler@hotmail.com>
2022-07-06 15:06:23 -0300

    Ajustes

---
+++ Fundamentos/Bloco_7/Dia_1/script2.js
@@ -0,0 +1,8 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+console.log(oddsAndEvens)
+
+const sortedArray = oddsAndEvens.sort(function(_a, _b) oddsAndEvens => {return b - a});
+
+console.log(sortedArray); // será necessário alterar essa linha 😉
+
+numbers.sort((a, b) => a - b);
--- Fundamentos/Dia_1/scritpt2.js
+++
@@ -1,9 +0,0 @@
-const Checker = (myNumber, number) => myNumber === number;
-
-const lotteryResult = (myNumber, callback) => {
-    const number = Math.floor((Math.random() * 5) + 1);
-
-    return callback(myNumber, number) ? 'YEAH' : 'Try Again!'
-};
-
-console.log(lotteryResult(5, Checker));
